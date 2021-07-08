c23c43277ff403575880741efa4ba97d5db3ddbb
Raymond Chan <raymond@9gag.com>
2021-07-08 23:26:30 +0800

    Init

---
+++ ExampleiOS/ExampleApp/ExampleApp/ExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleAppApp.swift
+//  ExampleApp
+//
+//  Created by raymond on 8/7/2021.
+//
+
+import SwiftUI
+
+@main
+struct ExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
